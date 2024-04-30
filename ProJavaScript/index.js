a0ddf2c8a2d55d64848d2e1adbb8b178a9470393
Sahil3982 <sahilpatel3982@gmail.com>
2024-04-30 19:31:58 +0530

    Update index.js

--- ProJavaScript/index.js
+++ ProJavaScript/index.js
@@ -1,30 +1,24 @@
+const image = document.querySelector("img");
+const btn = document.querySelector("button");
 
-const image = document.querySelector('img')
-const btn = document.querySelector('button')
+btn.addEventListener("click", () => {
+  // fetch('https://dog.ceo/api/breeds/image/random')
+  // .then(res => res.json())
+  // .then(json => {
+  //     console.log(json.message);
+  //     image.src = json.message
+
+  // })
+
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", "https://dog.ceo/api/breeds/image/random");
+  xhr.send();
+  xhr.addEventListener("load", () => {
+    console.log(xhr);
+    const json = JSON.parse(xhr.response);
+    console.log(json);
+    image.src = json.message;
+  });
 
 
-btn.addEventListener('click',()=>{
-    // fetch('https://dog.ceo/api/breeds/image/random')
-    // .then(res => res.json())
-    // .then(json => {
-    //     console.log(json.message);
-    //     image.src = json.message
-    
-    // })
-
-
-    const xhr = new XMLHttpRequest()
-
-    xhr.addEventListener('load', ()=>{
-        console.log(xhr);
-        const json = JSON.parse(xhr.response)
-        console.log(json);
-        image.src = json.message
-
-    })
-
-    xhr.open('GET','https://dog.ceo/api/breeds/image/random')
-    xhr.send()
-
-})
-
+});
