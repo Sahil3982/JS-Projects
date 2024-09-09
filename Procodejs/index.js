3bfd2557eb1d2f53b9cd09e5bc186898a52a46b5
Sahil3982 <sahilpatel3982@gmail.com>
2024-09-10 01:24:02 +0530

    .

---
+++ Procodejs/index.js
@@ -0,0 +1,32 @@
+// const num = [1, 2, 4, 5, 6, 8]
+
+// const value = num.some((a,i) => {
+//    console.log(10+1,i);
+   
+//     return a > 7
+// })
+
+// console.log(value);
+
+// let element=0
+// function add(...b){  //spread operator
+// for (let index = 0; index < b.length; index++) {
+//      element = element + b[index];
+    
+// } 
+    
+// console.log(element);
+
+// }
+
+// // rest operator
+// let arr= [1,2,3,45,6]
+// let prr = [1,2,3,4,5]
+// let mrr = [...arr,34,57,...prr]
+
+// console.log(mrr);
+// arguments.length
+// arguments.length
+// setInterval( ()=>{
+//     location.reload()
+// },1000)
