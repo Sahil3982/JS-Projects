2b1bf808d998b48b2b266ae2dc739758b1903823
Sahil3982 <sahilpatel3982@gmail.com>
2024-10-05 23:08:20 +0530

    basic of native

---
+++ ReactNative/Wallpaper/app/index.tsx
@@ -0,0 +1,23 @@
+import { Button, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+export default function () {
+  const [count, setcount] = useState(0);
+
+  useEffect(() => {
+    let time = setInterval(() => {
+      setcount(count => count + 1);
+    }, 1000);
+ return ()=>{
+    clearInterval(time);
+
+ }
+  },[]);
+  return (
+    <View>
+      <Button title="Login"></Button>
+      <Text>{count}</Text>
+      <Button title="Stop" ></Button>
+      <Button title="Singup" color={"green"}></Button>
+    </View>
+  );
+}
